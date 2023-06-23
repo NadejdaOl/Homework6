f7f0f08ea92d4e3f3013bc7702d439a6f36478ea
NadejdaOl <135122814+NadejdaOl@users.noreply.github.com>
2023-06-23 11:58:13 +0200

    Add files via upload
    Initial commit

---
+++ Homework_6/scripts/scripts.js
@@ -0,0 +1,28 @@
+//const numValue = +prompt ("Write a number");
+//console.log(numValue / 10);
+
+const firstNum = +prompt("Write a number!");
+const secondNum = +prompt("Write a number!");
+if (firstNum > secondNum) {;
+    console.log(secondNum);
+} else if(firstNum < secondNum) {;
+    console.log(firstNum);
+} 
+
+//const numValue = +prompt("Write a number!");
+//if (numValue > 100) {;
+ //   console.log("Число больше 100");
+//} else if(numValue < 100) {;
+  //  console.log("Число меньше 100");
+//} else if(numValue == 100) {;
+  //  console.log("Число равно 100");
+//};
+
+
+//const name = prompt("Введите ваше имя");
+//const age = prompt("Введите ваш возраст");
+//if (age > 18) {;
+  //  console.log(`Hello, ${name}`);
+//} else if(age < 18) {;
+  //  console.log(`Hi, ${name}`);
+//};
